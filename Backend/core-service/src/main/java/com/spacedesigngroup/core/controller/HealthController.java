aa699aba66e03e0bb2422dc0fef1b2e4158f4541
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-07-20 19:44:55 +0200

    clonejon

---
+++ Backend/core-service/src/main/java/com/spacedesigngroup/core/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.spacedesigngroup.core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/public/health")
+    public String health() {
+        return "ok";
+    }
+}
